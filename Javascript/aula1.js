b48bcaab6231e64abe275f1e831a99df729dcfa0
Emanuelle Siqueira <emanuelle.siqr@gmail.com>
2024-07-08 20:12:34 -0400

    Aula inical de JS(Revisar)

---
+++ Javascript/aula1.js
@@ -0,0 +1,57 @@
+//const varialvel1 = 1 //number
+//const vaeriavel2 = "texto" //string
+//const boleano = true //boolean true ou false
+//const nulo = null;
+//const undefined;
+//
+//const  objeto = {
+//    nome: "Emanuelle"
+//    idade: 20
+//}
+//
+//objeto.nome = "Fabar"
+//
+//const array = ["teste", 6527, true]
+//
+//const soma = 1 + 1;
+//const subtracao = 3 -1;
+//const multiplicacao = 2 * 1;
+//const divisao = 4/2;
+
+//let variavel_1 = 'dsdsds';
+//
+//function teste() {
+//    variavel_1 = 'mudou';
+//}
+//teste();
+//console.log(variavel_1)
+
+//var variavelGlobal = '';
+//
+//function teste() {
+//    var variavelGlobal = 'oi';
+//    teste2();
+//}
+//
+//function teste2 () {
+//    variavelGlobal = 'tchau';
+//}
+//
+//teste();
+//console.log(variavelGlobal);
+
+const aluno = 'Emanuelle'
+if(aluno == 'Emanuelle') {
+    console.log('Emanuelle');
+}
+else {
+    console.log('Fabar');
+}
+
+const numero = 22;
+if (numero === '22'){
+    console.log('Ele é igual a 22');
+}
+else {
+    console.log('Não eh...');
+}
