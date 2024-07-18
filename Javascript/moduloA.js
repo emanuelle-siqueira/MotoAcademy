e8c870bf9e27b0e44b675182098009b9c14f4df5
Emanuelle Siqueira <emanuelle.siqr@gmail.com>
2024-07-17 20:09:42 -0400

    Aula JS, continuação assunto modulo

---
+++ Javascript/moduloA.js
@@ -0,0 +1,24 @@
+//const nome = "Emanuelle"
+//const idade = 21;
+//
+//function soma(a,b) {
+//    return a+b;
+//}
+//
+//const PI = 3.14;
+//
+//function areaCirculo(raio){
+//    return PI * raio * raio;
+//}
+//
+//module.exports = {
+//    nome,idade,soma, areaCirculo, PI
+//}
+
+const pessoa = {
+    nome: "Emanu",
+    idade: 21,
+    cidade: "Manaus"
+}
+
+module.exports = pessoa
---
+++ Javascript/moduloB.js
@@ -0,0 +1,18 @@
+//const {nome, idade, soma, areaCirculo, PI} = require('./moduloA')
+////const moduloA = require('./moduloA')
+//
+//console.log(nome);
+//console.log(idade);
+//console.log(soma(2,2));
+//
+//console.log(areaCirculo(2));
+//console.log(PI);
+//
+////console.log(moduloA);
+
+const moduloA = require('./moduloA');
+
+console.log(moduloA);
+console.log(moduloA.nome);
+console.log(moduloA.idade);
+console.log(moduloA.cidade);
